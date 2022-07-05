196aae3f9c8b76cb37a30f982345cb0ea1046552
alice1530 <alice1530@163.com>
2022-07-05 17:29:21 +0800

    fix big bug,date is static bug

--- src/main/java/com/alice/config/MyScheduleTask.java
+++ src/main/java/com/alice/config/MyScheduleTask.java
@@ -1,46 +1,39 @@
 package com.alice.config;
 
-import com.alice.handler.CreateHtml;
-import com.alice.handler.DownloadM3u8;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import javax.annotation.PostConstruct;
+import java.text.SimpleDateFormat;
 
 @Configuration
 @EnableScheduling
 public class MyScheduleTask extends CommonBean {
 
 
-
-
-
-
     @Scheduled(cron = "${qfdj.task_cron}")
-    private void run(){
+    private void run() {
         try {
-	    log.info("定时任务开始......");
+            long begin = System.currentTimeMillis();
+            SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            log.info("[{}]定时任务开始......",f.format(begin));
             downloadM3u8.handle();
             //删除前几天的文件
             downloadM3u8.deleteNdays();
             //生成Html页面
             createHtml.createHtml();
-	    log.info("定时任务结束!");
+            long end = System.currentTimeMillis();
+            log.info("[{}]定时任务结束!耗时:{}ms",f.format(end),end-begin);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    //@PostConstruct启动时执行一次
+    //启动时执行一次
     @PostConstruct
-    private void startRunOnce(){
-        new Thread(()->{
+    private void startRunOnce() {
+        new Thread(() -> {
             run();
         }).start();
     }
